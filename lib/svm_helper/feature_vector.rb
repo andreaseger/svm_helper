e79b6cf019a58d2b36b63a0acb905eefc8804e2e
Andreas Eger <dev@eger-andreas.de>
2013-05-03 17:04:59 +0200

    use proper objects for preprocessed data and feature vectors

--- lib/svm_helper/feature_vector.rb
+++ lib/svm_helper/feature_vector.rb
@@ -1,14 +1,17 @@
-require_relative 'interface_helper'
-#
-# FeatureVector interface
-#
-# @author Andreas Eger
-class FeatureVector < InterfaceHelper
-  attribute :word_data
-  attribute :classification
-  attribute :label
+class FeatureVector
+  attr_accessor :word_data, :classification, :label
+
+  def initialize(params={})
+    params.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
 
   def data
     word_data + classification
+  end
+
+  def == another
+    [:word_data, :classification, :label].map { |e| self.send(sym) == another.send(sym) }.all?
   end
 end
--- lib/svm_helper/preprocessed_data.rb
+++ lib/svm_helper/preprocessed_data.rb
@@ -1,10 +1,13 @@
-require_relative 'interface_helper'
-#
-# PreprocessedData interface
-#
-# @author Andreas Eger
-class PreprocessedData < InterfaceHelper
-  attribute :data
-  attribute :id
-  attribute :label
+class PreprocessedData
+  attr_accessor :data, :id, :label
+
+  def initialize(params={})
+    params.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+
+  def == another
+    [:data, :id, :label].map { |e| self.send(sym) == another.send(sym)}.all?
+  end
 end
