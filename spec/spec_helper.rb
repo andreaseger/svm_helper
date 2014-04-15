20f6262bbe63c21ce01627dcd1a4a22eeaf9636e
Andreas Eger <dev@eger-andreas.de>
2014-04-15 17:52:30 +0200

    add tagging helper stuff for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@ Bundler.require(:default, :test)
 require 'svm_helper'
 
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir["./spec/support/**/*.rb"].each {|f| require f}
